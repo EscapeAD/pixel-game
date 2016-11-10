cb8439fcfab1c963f7831f7faab59644dcde0e09
Adam Tse <mr.adam.tse@gmail.com>
2016-11-10 15:32:56 -0500

    created style and sheet

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,8 +1,9 @@
 $(document).on('turbolinks:load', function(){
 
 $('#start_button').on('click', function(e){
+  var donut = $('li').add('text')
   e.preventDefault();
-  $('#game_list')
+  $('#game_list').append('<button>')
   console.log('test');
 })
 
