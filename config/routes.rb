74766f52caf143fab4b534909cb74d121065af3a
Adam Tse <mr.adam.tse@gmail.com>
2016-11-16 16:54:38 -0500

    added json response for devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
       resources :games
-      devise_for :users
-
+      devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
