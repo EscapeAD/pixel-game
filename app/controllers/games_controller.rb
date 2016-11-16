9061f273e9275cfd247bbecad28718edff7735f2
Adam Tse <mr.adam.tse@gmail.com>
2016-11-16 16:13:23 -0500

    revamped api minus control

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,51 @@
 class GamesController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
-  def create
-    button = {
-      name: params[:name]
-    }
+  # GET /users
+  def index
+    @games = Game.all
 
-    GameCreateJob.perform_later(button)
+    render json: @games
   end
 
+  # GET /users/1
+  def show
+    render json: @game
+  end
+
+  # POST /users
+  def create
+    @game = Game.new(user_params)
+
+    if @game.save
+      render json: @game, status: :created, location: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @game.update(user_params)
+      render json: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @game.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def game_params
+      params.require(:game).permit(:name)
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,16 @@ module Pixel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+      Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
+        allow do
+          origins '*'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+          end
+        end
+
   end
 end
