08b9559fe9341bea6e5ec7054c22479bf8fff2e9
Don Wong <donaldwong.apps@gmail.com>
2016-11-16 17:50:06 -0700

    Ionic is now talking to /test via http - OK, next steps: Impement ActionCable-Js

--- public/ion-pixel/src/pages/page1/page1.ts
+++ public/ion-pixel/src/pages/page1/page1.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
+
+import 'rxjs/Rx';
 
 @Component({
   selector: 'page-page1',
@@ -8,8 +10,33 @@ import { NavController } from 'ionic-angular';
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(
+    public navCtrl: NavController, 
+    private _http: Http 
+    ) {
   }
 
+  testCable() {
+     console.log( 'sending test post request to rails /test...' );
+     let h = new Headers();
+      h.append('Content-Type', 'application/json');
+
+      // Angular 2 Http Request Returns Observables using RxJs. 
+      // Check out Reactives Documentation on how to chain methods on the response.
+      this._http
+        .post(
+            "http://localhost:3000/test",
+            { data: "test" },
+            { headers:h }
+        )
+        .map(res => res.json())
+        .subscribe(
+            data => this.handleTestResponse(data),
+            error => console.log(error)
+        );
+      } 
+
+   handleTestResponse( data ) {
+     console.log( 'Handling The /test Response from Rails...', data );
+   }
 }
