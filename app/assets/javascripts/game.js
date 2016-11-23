f209de281c926dfa2f2c5478829aa04661bd0284
Adam Tse <mr.adam.tse@gmail.com>
2016-11-22 22:42:59 -0500

    comment out some old code

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,15 +1,15 @@
-$(document).on('turbolinks:load', function(){
-
-$('#start_button').on('click', function(e){
-  e.preventDefault();
-  $.ajax({
-    url: "/",
-    method: 'post',
-    data: {name: "test"},
-    dataType: 'json'
-  })
-})
-
-
-
-});
+// $(document).on('turbolinks:load', function(){
+//
+// $('#start_button').on('click', function(e){
+//   e.preventDefault();
+//   $.ajax({
+//     url: "/",
+//     method: 'post',
+//     data: {name: "test"},
+//     dataType: 'json'
+//   })
+// })
+//
+//
+//
+// });
